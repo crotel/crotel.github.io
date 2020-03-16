4ffcbfb000eb41fe0a609c36b699807fe89f6408
CROTEL <crotel@users.noreply.github.com>
2020-03-16 17:31:47 +0800

    fix

--- main.js
+++ main.js
@@ -31,15 +31,37 @@
                  return [year, month, day].join('-');
              }
              var publishTime = formatDate(json.posts[i].published_at);
-             var plaintext = json.posts[i].plaintext.replace(/\n+\n/g, '<br><br>');
-             fgpList.innerHTML += `
-<li><div class="content"><h3>${json.posts[i].title}</h3><p>${plaintext}</p></div><div class="point"></div><div class="date"><h4>${publishTime} </h4></div></li>`;
+             var filterUrl = /((www\.)?[a-zA-Z0-9_]{1,}(\-)?[a-zA-Z0-9_]{1,}\.[a-z]{2,}\b([a-zA-Z0-9@:%_\+.~#?&//=]*)|(www\.)?[a-zA-Z0-9_]{1,}\.[a-z]{2,}\b([a-zA-Z0-9@:%_\+.~#?&//=]*))/g;
+             var filterTodo = /\?+\?/g;
+             var filterUpdate = /\++\+/g;
+             var filterPs = /\!+\!/g;
+             var preRLB1 = json.posts[i].plaintext.replace(filterTodo, '<span class="todo"></span>');
+             var preRLB2 = preRLB1.replace(filterUpdate, '<span class="update"></span>');
+             var preRLB3 = preRLB2.replace(filterPs, '<span class="attention"></span>');
+             var replaceLineBreak = preRLB3.replace(/\n+\n/g, '<br><br>');
+             var getTitleUrl = new RegExp(filterUrl);
+             var plaintext, titleUrl;
+             
+             function gUrl(){
+                 var r = replaceLineBreak
+                 var m = replaceLineBreak.match(getTitleUrl);
+                 var p = plaintext;
+                 var t = titleUrl;
+                 if (m != null) {
+                     p = r.replace(filterUrl, '');
+                     t = m.toString();
+                     fgpList.innerHTML += `
+<li><div class="content"><h3><a href="https://${t}">${json.posts[i].title}<span>i</span></a></h3><p>${p}</p></div><div class="point"></div><div class="date"><h4>${publishTime} </h4></div></li>`;
+                     console.log(p,t);
+                     return;
+                 } else {
+                     p = r;
+                     fgpList.innerHTML += `
+<li><div class="content"><h3>${json.posts[i].title}</h3><p>${p}</p></div><div class="point"></div><div class="date"><h4>${publishTime} </h4></div></li>`;
+                     console.log(p);
+                     return;
+                 }
+             }
+             gUrl();
          }
      })
-     .catch(function (error) {
-         var p = document.createElement('p');
-         p.appendChild(
-             document.createTextNode('Error: ' + error.message)
-         );
-         document.body.insertBefore(p, fgpList);
-     });
