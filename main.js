975231114b686a93986054fa7d567ed91239c92d
CROTEL <crotel@users.noreply.github.com>
2020-03-17 20:08:19 +0800

    debug
    remove console.log and superfluous script

--- main.js
+++ main.js
@@ -4,7 +4,6 @@
      document.getElementById("header").setAttribute('style', 'display:none;');
      document.getElementById("content").setAttribute('style', 'display:block;padding-top:10vh;');
      document.getElementById("footer").setAttribute('style', 'display:block;');
-
  });
  fetch('https://cro.tel/data/')
      .then(function (response) {
@@ -16,32 +15,24 @@
      .then(function (json) {
          for (var i = 0; i < json.posts.length; i++) {
              var fgpList = document.querySelector('.timeline ul');
-
              function formatDate(date) {
                  var d = new Date(date),
                      month = '' + (d.getMonth() + 1),
                      day = '' + d.getDate(),
                      year = d.getFullYear();
-
                  if (month.length < 2)
                      month = '0' + month;
                  if (day.length < 2)
                      day = '0' + day;
-
                  return [year, month, day].join('-');
              }
              var publishTime = formatDate(json.posts[i].published_at);
-             var filterUrl = /((www\.)?[a-zA-Z0-9_]{1,}(\-)?[a-zA-Z0-9_]{1,}\.[a-z]{2,}\b([a-zA-Z0-9@:%_\+.~#?&//=]*)|(www\.)?[a-zA-Z0-9_]{1,}\.[a-z]{2,}\b([a-zA-Z0-9@:%_\+.~#?&//=]*))/g;
-             var filterTodo = /\?+\?/g;
-             var filterUpdate = /\++\+/g;
-             var filterPs = /\!+\!/g;
-             var preRLB1 = json.posts[i].plaintext.replace(filterTodo, '<span class="todo"></span>');
-             var preRLB2 = preRLB1.replace(filterUpdate, '<span class="update"></span>');
-             var preRLB3 = preRLB2.replace(filterPs, '<span class="attention"></span>');
+             var preRLB1 = json.posts[i].plaintext.replace(/\?+\?/g, '<span class="todo"></span>');
+             var preRLB2 = preRLB1.replace(/\++\+/g, '<span class="update"></span>');
+             var preRLB3 = preRLB2.replace(/\!+\!/g, '<span class="attention"></span>');
              var replaceLineBreak = preRLB3.replace(/\n+\n/g, '<br><br>');
-             var getTitleUrl = new RegExp(filterUrl);
+             var getTitleUrl = new RegExp(/((www\.)?[a-zA-Z0-9_]{1,}(\-)?[a-zA-Z0-9_]{1,}\.[a-z]{2,}\b([a-zA-Z0-9@:%_\+.~#?&//=]*)|(www\.)?[a-zA-Z0-9_]{1,}\.[a-z]{2,}\b([a-zA-Z0-9@:%_\+.~#?&//=]*))/g);
              var plaintext, titleUrl;
-             
              function gUrl(){
                  var r = replaceLineBreak
                  var m = replaceLineBreak.match(getTitleUrl);
@@ -52,13 +43,11 @@
                      t = m.toString();
                      fgpList.innerHTML += `
 <li><div class="content"><h3><a href="https://${t}">${json.posts[i].title}<span>i</span></a></h3><p>${p}</p></div><div class="point"></div><div class="date"><h4>${publishTime} </h4></div></li>`;
-                     console.log(p,t);
                      return;
                  } else {
                      p = r;
                      fgpList.innerHTML += `
 <li><div class="content"><h3>${json.posts[i].title}</h3><p>${p}</p></div><div class="point"></div><div class="date"><h4>${publishTime} </h4></div></li>`;
-                     console.log(p);
                      return;
                  }
              }
