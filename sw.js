21bfc431ac986148cdee733c33073f002ba7bc46
CROTEL <crotel@users.noreply.github.com>
2020-09-15 16:17:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,9 +4,10 @@ this.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_VERSION).then(function (cache) {
             return cache.addAll([
-                'https://cdn.jsdelivr.net/gh/crotel/dev@fbbfefb/style.css',
-                'https://cdn.jsdelivr.net/gh/crotel/dev@fbbfefb/font.css',
-                'https://cdn.jsdelivr.net/gh/crotel/dev@fbbfefb/main.js'
+                'favicon.ico',
+                'style.min.css',
+                'font.min.css',
+                'main.min.js'
             ]).catch(function (error) {
                 console.error('Error in install handler:', error);
             });
